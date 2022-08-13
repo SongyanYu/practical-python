819ccbd400d3ca62a679b808c6e58306e0d1b007
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2022-08-13 23:26:15 +1000

    first class

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+bounce = 1
+bounce_height = 100.0
+
+while bounce <= 10:
+    bounce_height = bounce_height * 3/5
+    print(bounce, round(bounce_height, 4))
+    bounce = bounce + 1
