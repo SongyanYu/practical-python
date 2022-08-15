c0cbd7e1ac4f7b3d277c82cf2230a46e91627ef5
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2022-08-15 21:53:38 +1000

    file class

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f)
+cost = 0
+
+for line in f:
+    row = line.split(',')
+    shares = int(row[1]) 
+    price = float(row[2].split(',')[0])
+    cost = shares * price + cost
+    
+f.close()
+print('Total cost', cost)
